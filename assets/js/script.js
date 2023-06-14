f559134bbb8a8e6f376dd145af089ca7c4048412
Veysel Aydin <weycell.aydin09@gmail.com>
2023-06-14 09:28:52 +0300

    back btn finish

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,3 +56,13 @@ const hideHeader = function () {
 
   lastScrollPos = window.scrollY;
 };
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 50) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+    hideHeader();
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
