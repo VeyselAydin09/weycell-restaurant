7fb9a37d29326cd1bde5bca65caaca4b5845624b
Veysel Aydin <weycell.aydin09@gmail.com>
2023-06-14 09:26:16 +0300

    js navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,19 @@ const addEventOnElements = function (elements, eventType, callback) {
     elements[i].addEventListener(eventType, callback);
   }
 };
+
+/**
+ * NAVBAR
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+};
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
